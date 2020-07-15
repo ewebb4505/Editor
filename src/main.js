8d0603504ca305876f50b425ed5478bcf0731592
ewebb4505 <ewebb4505@gmail.com>
2020-07-15 16:13:29 -0400

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,79 @@
+const tron = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const { electron } = require('process');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, '../templates/main.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function(){
+  createWindow()
+
+  //TODO: add an edit menu for this menu bar
+  const template = [
+    {
+      label: 'demo',
+      submenu: [
+        {
+          label: "subMenu1",
+          click: function(){
+            console.log("clicked submenu 1")
+          }
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "subMenu2"
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      click: function(){
+        tron.shell.openExternal('https://electron.atom.io')
+      }
+    }
+  ]
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
